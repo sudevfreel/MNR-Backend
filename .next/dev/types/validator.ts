be98671b41ed0139658de8d765c118c80996ed07
Sudevkumar123 <sudev.dev@evfy.in>
2026-07-16 13:39:34 +0530

    Api added.

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -92,6 +92,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../../src/app/api/webhooks/whatsapp/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/webhooks/whatsapp">> = Specific
+  const handler = {} as typeof import("../../../src/app/api/webhooks/whatsapp/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 
 
 
