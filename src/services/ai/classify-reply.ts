aafc0adc0fdc4283933fdcc56e832448b384c6aa
Sudevkumar123 <sudev.dev@evfy.in>
2026-07-18 18:37:25 +0530

    Refctor

--- src/services/ai/classify-reply.ts
+++ src/services/ai/classify-reply.ts
@@ -78,7 +78,7 @@ ${employeeReply}
     console.log('2. Calling Gemini')
 
     const response = await gemini.models.generateContent({
-      model: 'gemini-2.5-flash', //Done
+      model: 'gemini-3.5-flash', //Done
       contents: prompt,
     })
 
